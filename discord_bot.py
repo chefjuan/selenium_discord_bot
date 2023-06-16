7ce4da08a7aeb0f5cd87aac35c1a3bab479d36b4
chefjuan <92621652+chefjuan@users.noreply.github.com>
2023-06-16 18:54:43 +0200

    Create discord_bot.py

---
+++ discord_bot.py
@@ -0,0 +1,58 @@
+import discord
+import time
+
+
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!travel'):
+        await message.channel.send('Chwila...')
+
+    if message.content.startswith('!info'):
+        await message.channel.send('Zamiast spacji używaj znaku podłogi. Przykładowo: Wierzbowa_41 lub Stadion_Miejski')
+
+    # Sprawdź, czy wiadomość zaczyna się od !travel i zawiera dwa argumenty oddzielone spacją
+    if message.content.startswith('!travel'):
+        args = message.content.split(' ')
+        if len(args) == 3:
+            arg1 = args[1]
+            arg2 = args[2]
+            print(arg1)
+            print(arg2)
+            from response import travel_response
+            response = travel_response(arg1,arg2)
+            await message.channel.send(response)
+
+
+        if len(args) > 3:
+            await message.channel.send('Wystąpił bład. Spróbuj ponownie lub użyj komendy !info w celu uzyskania informacji')
+
+    if message.content.startswith('!travel_next'):
+        args = message.content.split(' ')
+        if len(args) == 3:
+            arg1 = args[1]
+            arg2 = args[2]
+            print(arg1)
+            print(arg2)
+            from response_next import travel_response
+            response = travel_response(arg1,arg2)
+            await message.channel.send(response)
+
+
+TOKEN = 'MTA5NzQ5NDI0ODg4NDAyMzMyNg.GAiOR8.GJ-b_J58ao_yDb6hY45A_U5NnA8ctfe_cUt5zs'
+client.run('MTA5NzQ5NDI0ODg4NDAyMzMyNg.GAiOR8.GJ-b_J58ao_yDb6hY45A_U5NnA8ctfe_cUt5zs')
+
+while True:
+    pass
+
+
+
+
+
+
+
+
+
